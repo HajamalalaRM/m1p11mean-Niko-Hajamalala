d573a35ef1dc053a51921f07d4092f1d9ad658b6
HajamalalaRM <80814773+HajamalalaRM@users.noreply.github.com>
2024-02-29 19:13:19 +0400

    Rectification baseurl

--- src/app/BaseUrl.ts
+++ src/app/BaseUrl.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class BaseUrl {
    // private baseUrl = 'http://192.168.43.119:3000/';
-   private baseUrl = 'https://m1p11mean-niko-hajamalala-back.onrender.com:3000';
+   private baseUrl = 'https://m1p11mean-niko-hajamalala-back.onrender.com';
    // private baseUrl = 'http://localhost:9008/api/v1';
 
    getBaseUrl(){
